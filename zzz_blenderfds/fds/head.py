b3612a48a8bd2f92c5a066986bdd58a2144fc717
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-01-24 09:33:12 +0100

    slight refactoring

--- zzz_blenderfds/fds/head.py
+++ zzz_blenderfds/fds/head.py
@@ -2,17 +2,24 @@
 
 import bpy
 
+DEBUG = True
+
 def set_free_text_file(context, scene):
-    """Set free text file name, then try to open it in shown Blender Text Editor area."""
-    # Check name existence
+    """Get or set unique free text file name, open it in shown Blender Text Editor area."""
+    # Name?
     if not scene.bf_head_free_text:
-        # Empty field
-        scene.bf_head_free_text = "HEAD free text ({})".format(scene.name)
-    # Check text existence
-    if scene.bf_head_free_text not in bpy.data.texts:
-        # No linked file, create
+        # No free text name, find and set unique name
+        bf_head_free_text = bf_head_free_text_new = "HEAD free text"
+        i = 0
+        while bf_head_free_text_new in bpy.data.texts:
+            i += 1
+            bf_head_free_text_new = ".".join((bf_head_free_text, str(i)))
+        scene.bf_head_free_text = bf_head_free_text_new
+    # File?
+    if not scene.bf_head_free_text in bpy.data.texts:
+        # No file: create file
         bpy.data.texts.new(scene.bf_head_free_text)
-    # Search for the Text Editor
+    # Search for the Text Editor area in Blender UI
     area_te = None
     for window in context.window_manager.windows:
         for area in window.screen.areas:
@@ -24,4 +31,4 @@ def set_free_text_file(context, scene):
         area_te.spaces[0].text = bpy.data.texts[scene.bf_head_free_text]
     # Return free text file name
     return scene.bf_head_free_text
-
+    
