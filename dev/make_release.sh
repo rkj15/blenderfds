c89f9ae9c81935e79f363cf41d1b128bc4a678fd
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-01-25 23:13:00 +0100

    Quick bash script for release building

---
+++ dev/make_release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+version=v4.0.0
+echo "Making BlenderFDS version $version release..."
+
+short_version=${version:1}
+
+if wget https://github.com/firetools/blenderfds/archive/$version.zip
+then
+    echo "Version $version downloaded!"
+else
+    echo "Cannot download version $version"
+    exit 1
+fi
+
+unzip $version.zip
+rm -rf $version.zip
+
+mv blenderfds-$short_version BlenderFDS-$version
+cd BlenderFDS-$version
+zip -r blenderfds.zip zzz_blenderfds
+rm -rf zzz_blenderfds .gitignore
+
+cd ..
+zip -r BlenderFDS-$version.zip BlenderFDS-$version
+rm -rf BlenderFDS-$version
+mv BlenderFDS-$version.zip ~/
+
+echo
+echo "Release BlenderFDS-$version.zip is ready!"
+
