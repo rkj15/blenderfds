b8c423b0e6154108d3dc51bb51a7a95af10c4882
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-02-03 14:23:01 +0100

    Cosmetic fix

--- zzz_blenderfds/fds/head.py
+++ zzz_blenderfds/fds/head.py
@@ -19,16 +19,12 @@ def set_free_text_file(context, scene):
     if not scene.bf_head_free_text in bpy.data.texts:
         # No file: create file
         bpy.data.texts.new(scene.bf_head_free_text)
-    # Search for the Text Editor area in Blender UI
-    area_te = None
+    # Search for the Text Editor area in Blender UI. If found, show requested text
     for window in context.window_manager.windows:
         for area in window.screen.areas:
-            if 'TEXT_EDITOR' == area.type:
-                area_te = area
+            if area.type == 'TEXT_EDITOR':
+                area.spaces[0].text = bpy.data.texts[scene.bf_head_free_text]
                 break
-    # If Text Editor is displayed, show requested text
-    if area_te:
-        area_te.spaces[0].text = bpy.data.texts[scene.bf_head_free_text]
     # Return free text file name
     return scene.bf_head_free_text
     
