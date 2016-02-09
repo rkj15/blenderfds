f190197226bc7abb337a975ee3f365c3d1f2f4b7
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-02-09 21:57:01 +0100

    fix

--- dev/make_release.sh
+++ dev/make_release.sh
@@ -1,32 +1,60 @@
 #!/bin/bash
+# Make BlenderFDS release <http://blenderfds.org/>.
+# Copyright (C) 2016 Emanuele Gissi
 
-version=v4.0.2
-echo "Making BlenderFDS version $version release..."
+# Setup
 
-short_version=${version:1}
+tag="v4.0.0"
+short_tag=${tag:1}
+release_name="BlenderFDS-$tag"
 
-if wget https://github.com/firetools/blenderfds/archive/$version.zip
-then
-    echo "Version $version downloaded!"
+github_url="https://github.com/firetools/blenderfds/archive"
+github_file="$tag.zip"
+github_unzip_dir="blenderfds-$short_tag"
+
+git_local="/home/egissi/Documenti/Argomenti/BlenderFDS/git"
+
+destination_path="/home/egissi/Documenti/Argomenti/BlenderFDS/BlenderFDS_releases"
+
+# Main
+
+source colors.sh
+echo_title "Making <$release_name> release..."
+
+# Download from GitHub
+
+echo_msg "Downloading <$github_file> from GitHub..."
+cd /tmp
+if wget $github_url/$github_file; then echo_ok "Done."
 else
-    echo "Cannot download version $version"
+    echo_err "Cannot download <$github_file> from GitHub. Aborted."
     exit 1
 fi
 
-unzip blenderfds-$short_version.zip
-rm -rf blenderfds-$short_version.zip
-mv blenderfds-$short_version BlenderFDS-$version
+# Unzipping and renaming
 
-cd BlenderFDS-$version
-zip -r blenderfds.zip zzz_blenderfds
-rm -rf zzz_blenderfds dev .gitignore
+echo_msg "Unzipping and renaming <$github_file>..."
+if (
+    unzip $github_file && rm -rf $github_file
+    mv $github_unzip_dir $release_name
+); then echo_ok "Done."
+else
+    echo_err "Cannot unzip <$github_file>. Aborted."
+    exit 1
+fi
 
-cd ..
+# Preparing release
 
-zip -r BlenderFDS-$version.zip BlenderFDS-$version
-rm -rf BlenderFDS-$version
-mv BlenderFDS-$version.zip ~/
-
-echo
-echo "Release BlenderFDS-$version.zip is ready!"
-
+echo_msg "Preparing release <$release_name.zip> in <$destination_path>..."
+if (
+    cd $release_name
+    zip -r blenderfds.zip zzz_blenderfds && rm -rf zzz_blenderfds
+    rm -rf dev .gitignore
+    cd ..
+    zip -r $release_name.zip $release_name && rm -rf $release_name
+    mv $release_name.zip $destination_path
+); then echo_ok "Done."
+else
+    echo_err "Cannot prepare release. Aborted."
+    exit 1
+fi
