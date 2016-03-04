254d81555ee36bc77b352891b1fbb84042e1c866
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-03-04 13:29:13 +0100

    updated make_release.sh

--- dev/make_release.sh
+++ dev/make_release.sh
@@ -9,7 +9,7 @@ short_tag=${tag:1}
 release_name="BlenderFDS-$tag"
 
 github_url="https://github.com/firetools/blenderfds/archive"
-github_file="blenderfds-$short_tag.zip"
+github_file="$tag.zip"
 github_unzip_dir="blenderfds-$short_tag"
 
 git_local="/home/egissi/Documenti/Argomenti/BlenderFDS/git"
@@ -23,13 +23,13 @@ echo_title "Making <$release_name> release..."
 
 # Download from GitHub
 
-#echo_msg "Downloading <$github_file> from GitHub..."
-#cd /tmp
-#if wget $github_url/$github_file; then echo_ok "Done."
-#else
-#    echo_err "Cannot download <$github_file> from GitHub. Aborted."
-#    exit 1
-#fi
+echo_msg "Downloading <$github_file> from GitHub..."
+cd /tmp
+if wget $github_url/$github_file; then echo_ok "Done."
+else
+    echo_err "Cannot download <$github_file> from GitHub. Aborted."
+    exit 1
+fi
 
 # Unzipping and renaming
 
@@ -49,7 +49,7 @@ echo_msg "Preparing release <$release_name.zip> in <$destination_path>..."
 if (
     cd $release_name
     zip -r blenderfds.zip zzz_blenderfds && rm -rf zzz_blenderfds
-    rm -rf dev .gitignore
+    rm -rf dev
     cd ..
     zip -r $release_name.zip $release_name && rm -rf $release_name
     mv $release_name.zip $destination_path
@@ -58,3 +58,4 @@ else
     echo_err "Cannot prepare release. Aborted."
     exit 1
 fi
+
