fefa323603907cf38570f02fb9f02fd22a9a6a0f
Emanuele Gissi <emanuele.gissi@gmail.com>
2016-03-27 09:38:01 +0200

    None

--- dev/make_release.sh
+++ dev/make_release.sh
@@ -4,7 +4,7 @@
 
 # Setup
 
-tag="v4.0.5"
+tag="v4.0.6"
 short_tag=${tag:1}
 release_name="BlenderFDS-$tag"
 
@@ -21,15 +21,49 @@ destination_path="/home/egissi/Documenti/Argomenti/BlenderFDS/BlenderFDS_release
 source colors.sh
 echo_title "Making <$release_name> release..."
 
+# Run tests
+
+echo_msg "Running auto-tests..."
+if (
+    python3 ./make_test.py
+); then echo_ok "Done."
+else
+    echo_err "Test error reported. Aborted."
+    exit 1
+fi
+
+# Commit and push latest changes (gitg)
+
+echo_msg "Committing and pushing latest changes..."
+if (
+    gitg
+    git push
+); then echo_ok "Done."
+else
+    echo_err "Cannot commit and push. Aborted."
+    exit 1
+fi
+
+# Write release notes
+
+echo_msg "Writing release notes..."
+google-chrome "https://github.com/firetools/blenderfds/wiki/Release-notes/_edit"
+echo_ok "Done."
+
+# Tag pre-release
+
+git tag -a $tag -m "Release $tag"
+git push --tags
+
 # Download from GitHub
 
-#echo_msg "Downloading <$github_file> from GitHub..."
+echo_msg "Downloading <$github_file> from GitHub..."
 cd /tmp
-#if wget $github_url/$github_file; then echo_ok "Done."
-#else
-#    echo_err "Cannot download <$github_file> from GitHub. Aborted."
-#    exit 1
-#fi
+if wget $github_url/$github_file; then echo_ok "Done."
+else
+    echo_err "Cannot download <$github_file> from GitHub. Aborted."
+    exit 1
+fi
 
 # Unzipping and renaming
 
@@ -58,4 +92,14 @@ else
     echo_err "Cannot prepare release. Aborted."
     exit 1
 fi
+
+# Setup release
+
+echo_msg "Edit release page..."
+google-chrome "https://github.com/firetools/blenderfds/releases"
+echo_ok "Done."
+
+# Bump version
+
+echo_msg "Do not forget to bump version!"
 
